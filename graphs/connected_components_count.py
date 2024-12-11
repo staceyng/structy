b39f87c7a09a5a097a846955b48def333cc36ae8
staceyng <woodnotetheory@gmail.com>
2024-12-10 23:05:33 -0700

    feat: add graph problems 2 & 3

---
+++ graphs/connected_components_count.py
@@ -0,0 +1,28 @@
+def connected_components_count(graph):
+    count = 0
+    visited = set()
+
+    for node in graph:
+        if explore(graph, node, visited):
+            count += 1
+
+    return count
+
+
+def explore(graph, current, visited) -> bool:
+    if current in visited:
+        return False
+
+    visited.add(current)
+
+    for neighbour in graph[current]:
+        explore(graph, neighbour, visited)
+
+    return True
+
+
+if __name__ == "__main__":
+    res = connected_components_count(
+        {0: [8, 1, 5], 1: [0], 5: [0, 8], 8: [0, 5], 2: [3, 4], 3: [2, 4], 4: [3, 2]}
+    )  # -> 2
+    print(res)
---
+++ graphs/undirected_path.py
@@ -0,0 +1,46 @@
+def undirected_path(edges, node_A, node_B):
+    # convert edges to an adjacency list
+    graph = _build_graph(edges)
+    return has_path(graph, node_A, node_B, set())
+
+
+def has_path(graph, src, dst, visited):
+    # using depth first search - solve recursively
+    if src == dst:
+        return True
+
+    # check for infinite loop
+    if src in visited:
+        return False
+
+    visited.add(src)
+
+    for neighbour in graph[src]:
+        if has_path(graph, neighbour, dst, visited) is True:
+            # found path
+            return True
+
+    # looked through all neighbours and paths
+    return False
+
+
+def _build_graph(edges: list):
+    graph = {}
+    for edge in edges:
+        a, b = edge
+        if a not in graph:
+            graph[a] = []
+        if b not in graph:
+            graph[b] = []
+
+        graph[a].append(b)
+        graph[b].append(a)
+
+    return graph
+
+
+if __name__ == "__main__":
+    edges = [("i", "j"), ("k", "i"), ("m", "k"), ("k", "l"), ("o", "n")]
+
+    res = undirected_path(edges, "j", "m")
+    print(res)
